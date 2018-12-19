dc455c1e94fb0b33205c43e6adb755796bb21acf
Robbie <saucy.cooking@gmail.com>
2018-12-19 11:14:29 -0700

    Commit 2 - Added a function to stage the battle

--- js/main.js
+++ js/main.js
@@ -6,6 +6,7 @@ const hero = {
     hull: 20,
     firepower: 5,
     accuracy: .7,
+    isAlive: true,
     attack(target) {
         let ranNum = Math.random();
         console.log(`Accuracy threshold is ${ranNum}`);
@@ -14,6 +15,7 @@ const hero = {
             target.hull = target.hull - this.firepower;
             console.log(`alien has ${target.hull} hull points left.`);
             if (target.hull <= 0) {
+                target.isAlive = false;
                 console.log(`Alien ship is destroyed you have saved the Universe from complete destruction!!`);
 
             }
@@ -26,6 +28,7 @@ const alien = {
     hull: Math.round((Math.random() * (6 - 3)) + 3),
     firepower: Math.round((Math.random() * (4 - 2)) + 2),
     accuracy: (Math.random() * (.8 - .6)) + .6, //for later figure out a way to make it to the hundredth?
+    isAlive: true,
     attack(target) {
         let ranNum = Math.random();
         console.log(`Accuracy threshold is ${ranNum}`);
@@ -34,11 +37,20 @@ const alien = {
             target.hull = target.hull - this.firepower;
             console.log(`hero has ${target.hull} hull points left.`);
             if (target.hull <= 0) {
+                target.isAlive = false;
                 console.log(`You have died, your people will now be used as cattle for the CRAVERSSS!!! GRRZZT ZOIK!`);
 
             }
         } else {
             console.log(`You dodged the attack!`);
+        }
+    }
+}
+const battle = (player, computer) => {
+    while (player.isAlive && computer.isAlive) {
+        player.attack(computer);
+        if (computer.isAlive) {
+            computer.attack(player);
         }
     }
 }
